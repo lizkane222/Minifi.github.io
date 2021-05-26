2d8a0e6ff372d45dc8e7ec0087cb38552045c479
Liz Kane <liz.kane@segment.com>
2021-05-26 14:22:29 -0700

    snippet in gh-pages

--- controllers/motivate.js
+++ controllers/motivate.js
@@ -5,14 +5,16 @@ const router = express.Router();
 
 const db = require('../models');
 
+
 // index view
 router.get('/', async (req,res)=> {
     // res.send("Motivate's INDEX Route is ⇪ and 🏃🏻‍♀️");
     try {
-        const foundMotivate = await db.Motivate.find({})
-        const context = {motivations : foundMotivate}
-    res.render('motivate/index', context)
-
+        const foundMotivate = await db.Motivate.find({});
+        res.render('motivate/index', context = {
+            motivations : foundMotivate
+        }
+        )
     } catch (error) {
         console.log(error)
         return res.send({message: `Internal Server Error: check motivate controller @ index route ${error}`});
